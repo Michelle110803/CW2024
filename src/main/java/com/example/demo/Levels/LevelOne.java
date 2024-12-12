ef0056e505b498f95e13bfb77fe7ba07d52c69cd
Michelle110803 <michellealessandra118@gmail.com>
2024-12-12 23:00:55 +0800

    Revert "- group pictures to folders"
    This reverts commit 68fabf284188ddf4da0b9d2f448987894b315270.

--- src/main/java/com/example/demo/Levels/LevelOne.java
+++ src/main/java/com/example/demo/Levels/LevelOne.java
@@ -7,7 +7,7 @@ import com.example.demo.controller.Controller;
 
 public class LevelOne extends LevelParent {
 
-	private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background/levelOneBackground.png";
+	private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/levelOneBackground.png";
 	private static final String NEXT_LEVEL = "com.example.demo.Levels.LevelTwo";
 	private static final int TOTAL_ENEMIES = 5;
 	private static final int KILLS_TO_ADVANCE = 5;
