1ea673e1b5b4965789b7413b52c47045b4e31939
Michelle110803 <michellealessandra118@gmail.com>
2024-12-12 22:25:43 +0800

    - character updates

--- src/main/java/com/example/demo/projectiles/BossProjectile.java
+++ src/main/java/com/example/demo/projectiles/BossProjectile.java
@@ -5,7 +5,7 @@ import javafx.geometry.BoundingBox;
 
 public class BossProjectile extends Projectile {
 	
-	private static final String IMAGE_NAME = "BossProjectile.png";
+	private static final String IMAGE_NAME = "fireball.png";
 	private static final int IMAGE_HEIGHT = 60;
 	private static final int HORIZONTAL_VELOCITY = -15;
 	private static final int INITIAL_X_POSITION = 920;
